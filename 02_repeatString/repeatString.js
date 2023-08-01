08632c7cd51422b74a4247121ce27485fc39e3b6
WeamHassan <weamelelayany@gmail.com>
2023-08-01 21:09:13 +0300

    Add Solutions for exercise 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, repeatingNumber) {
 
+    if (repeatingNumber < 0)
+    {
+        return "ERROR";
+    }
+
+    let result = "";
+
+    for (let i = 0; i < repeatingNumber; i++)
+    {
+        result += word;    
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(wordToBeReversed) {
+    let wordToBeReversedArray;
+    let reversedWord = "";
+    wordToBeReversedArray = wordToBeReversed.split("");
+    
+    for(let i = wordToBeReversedArray.length - 1; i >= 0; i--)
+    {
+        reversedWord += wordToBeReversedArray[i];
+    }
 
+    console.log(reversedWord);
+    
 };
 
 // Do not edit below this line
